ed9000c0c2bf48c46193fe06569da812e71533c5
jameskiprop <jameskiprop01@gmail.com>
2024-08-11 22:06:35 +0300

    some progress on the Basic calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+//selecting the button elements
+const addButton = document.getElementsByClassName("add");
+const subtractButton = document.getElementsByClassName("subtract");
+const multiplyButton = document.getElementsByClassName("multiply");
+const divideButton = document.getElementsByClassName("divide");
+const equalButton = document.getElementsByClassName("equal");
+const deleteButton = document.getElementsByClassName("delete");
+
+//functions to determine the various arithmetic operations and make them happen
