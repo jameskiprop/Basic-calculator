bf3c6ba86cec3e485374c4b50450bcd8bc589708
jameskiprop <jameskiprop01@gmail.com>
2024-08-14 22:27:30 +0300

    project progress

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addButton = document.getElementsByClassName("add");
 const subtractButton = document.getElementsByClassName("subtract");
 const multiplyButton = document.getElementsByClassName("multiply");
 const divideButton = document.getElementsByClassName("divide");
-const equalButton = document.getElementsByClassName("equal");
+const equalsButton = document.getElementsByClassName("equal");
 const deleteButton = document.getElementsByClassName("delete");
 
 //functions to determine the various arithmetic operations and make them happen
